7543396e150f563d4da4a4be9f89a2212c85979f
ahdp861 <lkssaa862@gmail.com>
2025-06-06 14:14:37 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,23 +2,23 @@ using System;
 
 namespace RetailDatabaseManager
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string filePath = "LR5-var1.xls";
-            var manager = new DatabaseManager();
+      string filePath = "retail_database.xlsx";
+      var manager = new DatabaseManager();
 
-            try
-            {
-                manager.LoadDatabase(filePath);
-                manager.ShowMainMenu();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Критическая ошибка: {ex.Message}");
-                manager.Logger.Log($"Критическая ошибка: {ex.Message}");
-            }
-        }
+      try
+      {
+        manager.LoadDatabase(filePath);
+        manager.ShowMainMenu();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"Критическая ошибка: {ex.Message}");
+        manager.Logger.Log($"Критическая ошибка: {ex.Message}");
+      }
     }
+  }
 }
