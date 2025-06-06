3155f24707a129f4d2ce2e56f59acf31c01a4cc4
ahdp861 <lkssaa862@gmail.com>
2025-06-06 14:30:18 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,20 +2,22 @@ using System;
 
 namespace RetailDatabaseManager
 {
-  class Program
-  {
-    static void Main(string[] args)
-    {
+  /// <summary>
+  /// Основной класс программы, содержащий точку входа.
+  /// </summary>
+  class Program {
+    /// <summary>
+    /// Точка входа в программу. Создает экземпляр DatabaseManager и запускает основной цикл программы.
+    /// </summary>
+    /// <param name="args">Аргументы командной строки (не используются).</param>
+    static void Main(string[] args) {
       string filePath = "retail_database.xlsx";
       var manager = new DatabaseManager();
 
-      try
-      {
+      try {
         manager.LoadDatabase(filePath);
         manager.ShowMainMenu();
-      }
-      catch (Exception ex)
-      {
+      } catch (Exception ex) {
         Console.WriteLine($"Критическая ошибка: {ex.Message}");
         manager.Logger.Log($"Критическая ошибка: {ex.Message}");
       }
